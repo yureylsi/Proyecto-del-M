b22eb3a83dff0c6d46653daae1409ea2a3ab1f73
Yureylis <Yureylis@gmail.com>
2026-04-19 18:30:02 -0400

    Nuevo

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "registro");
+$conexion = new mysqli("localhost", "grupo2", "12345", "registro");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
